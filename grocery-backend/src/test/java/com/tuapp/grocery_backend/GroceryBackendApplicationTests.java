7336a1bc34daf091bfb94f0f4eadab9e683ce56a
Arejic <arelijico@gmail.com>
2026-03-22 17:50:06 -0600

    cambios base conexion

--- grocery-backend/src/test/java/com/tuapp/grocery_backend/GroceryBackendApplicationTests.java
+++ grocery-backend/src/test/java/com/tuapp/grocery_backend/GroceryBackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.tuapp.grocery_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class GroceryBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
